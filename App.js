afe3d34a409a3ce1ade1e07ebf4aff897f2a5c1d
chaoyuquenet <chaoyu1979@gmail.com>
2019-04-22 18:29:32 +0200

    feat(store): implement store into App.js

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
 import Navigation from './navigation/Navigation';
-
+import store from './store/configureStore';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <Provider store={store}>
         <Navigation />
-      </View>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
-    // alignItems: 'center',
-    // justifyContent: 'center'
-  }
-});

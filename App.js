42f45129aab9e7bfa9fd066ae75b4bbe33589931
chaoyuquenet <chaoyu1979@gmail.com>
2019-04-24 17:54:26 +0200

    feat(persist): implement persist

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { persistGate, PersistGate } from 'redux-persist/es/integration/react';
 
 import Navigation from './navigation/Navigation';
 import store from './store/configureStore';
+
 export default class App extends React.Component {
   render() {
+    let persistor = persistStore(store);
     return (
       <Provider store={store}>
-        <Navigation />
+        <PersistGate persistor={persistor}>
+          <Navigation />
+        </PersistGate>
       </Provider>
     );
   }
--- store/configureStore.js
+++ store/configureStore.js
@@ -1,4 +1,14 @@
 import { createStore } from 'redux';
+import { persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import toggleFavorite from './reducers/favoriteReducer';
 
-export default createStore(toggleFavorite);
+const rootPesistConfig = {
+  key: 'root',
+  storage: storage
+};
+
+export default createStore(
+  persistCombineReducers(rootPesistConfig, toggleFavorite)
+);
